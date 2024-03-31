3caddfd9b782395da2fbe0d8296366c9b1cbe2b1
Kimmm-c <97310663+Kimmm-c@users.noreply.github.com>
2024-03-30 20:06:13 -0700

    added retry policy to avoid huggingface api failure on the first few calls

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+const { default: axiosRetry } = require('axios-retry');
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Body parser middleware
+axiosRetry(axios, { retries: 3, retryDelay: axiosRetry.exponentialDelay });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
